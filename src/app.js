44dd8157fe2a120cdeb8daa89344c8d025c34bdc
unknown <sumajcoronel2002@gmail.com>
2022-12-09 16:33:55 -0300

    Agregando cosas

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
 
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
 
-
-
+/** MIDDLEWARES */
+app.use(morgan("dev"))
 app.use(express.json());
 
 /** ENDPOINTS */
 
-app.use(indexRoutes);
+app.use('/', indexRoutes);
 app.use('/api', employeesRoutes);
 
 app.use((req, res, next) => {
--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import {PORT} from './config.js';
 
 /** EL SERVIDOR ESTA CORRIENDO EN EL PUERTO 3306, MIENTRAS QUE LA BASE DE DATOS ESTA EN EL PUERTO 3000 */
 app.listen(PORT)
-console.log('Servidor corriendo en el puerto 3306')
+console.log(`Server corriendo en http://localhost:${PORT}`);
